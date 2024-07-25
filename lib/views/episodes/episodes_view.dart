bf0f0fbafb3ef202f8434b4bf12bccaed67e361b
Tarun-Rajan <ktarunrajan@gmail.com>
2024-07-25 15:57:16 +0530

    Update episodes_view.dart
    course buttons

--- lib/views/episodes/episodes_view.dart
+++ lib/views/episodes/episodes_view.dart
@@ -6,7 +6,30 @@ class EpisodesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.white, // Background color changed to white
+      child: Center(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Center the column vertically
+          children: [
+            SizedBox(height: 20), // Add some space at the top
+            ElevatedButton(
+              child: Text('course 1'),
+              onPressed: () {}, // Add button press handler
+            ),
+            SizedBox(height: 20), // Add space between buttons
+            ElevatedButton(
+              child: Text('course 2'),
+              onPressed: () {}, // Add button press handler
+            ),
+            SizedBox(height: 20), // Add space between buttons
+            ElevatedButton(
+              child: Text('course 3'),
+              onPressed: () {}, // Add button press handler
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
