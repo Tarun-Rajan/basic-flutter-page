ef54176f23417555bc9dbb5d96fc4a1e0fb9a474
Tarun-Rajan <ktarunrajan@gmail.com>
2024-07-25 19:35:34 +0530

    Update episodes_view.dart
    keys to left

--- lib/views/episodes/episodes_view.dart
+++ lib/views/episodes/episodes_view.dart
@@ -35,70 +35,101 @@ class _EpisodesViewState extends State<EpisodesView> {
                 ),
               ),
               SizedBox(height: _buttonSpacing), // Add some space at the top
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20), // Increase button padding
-                  textStyle: TextStyle(fontSize: 24), // Increase text size
-                  backgroundColor:
-                      Colors.purple, // Change button color to purple
-                ),
-                child: Text('Course 1'),
-                onPressed: () {
-                  setState(() {
-                    _selectedCourse = 'Course 1';
-                  });
-                }, // Add button press handler
-              ),
-              SizedBox(height: _buttonSpacing), // Add space between buttons
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20), // Increase button padding
-                  textStyle: TextStyle(fontSize: 24), // Increase text size
-                  backgroundColor:
-                      Colors.purple, // Change button color to purple
-                ),
-                child: Text('Course 2'),
-                onPressed: () {
-                  setState(() {
-                    _selectedCourse = 'Course 2';
-                  });
-                }, // Add button press handler
-              ),
-              SizedBox(height: _buttonSpacing), // Add space between buttons
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20), // Increase button padding
-                  textStyle: TextStyle(fontSize: 24), // Increase text size
-                  backgroundColor:
-                      Colors.purple, // Change button color to purple
-                ),
-                child: Text('Course 3'),
-                onPressed: () {
-                  setState(() {
-                    _selectedCourse = 'Course 3';
-                  });
-                }, // Add button press handler
-              ),
-              SizedBox(
-                  height: _buttonSpacing), // Add space between buttons and text
-              Text(
-                _selectedCourse.isEmpty
-                    ? ''
-                    : _selectedCourse == 'Course 1'
-                        ? 'This is Course 1. Learn the basics of programming.'
-                        : _selectedCourse == 'Course 2'
-                            ? 'This is Course 2. Learn advanced programming concepts.'
-                            : 'This is Course 3. Learn specialized programming skills.',
-                style: TextStyle(fontSize: 18),
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.end, // align to the right
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Add enroll button press handler
-                    },
-                    child: Text('Enroll'),
+                  Expanded(
+                    flex: 2,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(20), // Increase button padding
+                        textStyle:
+                            TextStyle(fontSize: 24), // Increase text size
+                        backgroundColor: Colors
+                            .purple[200], // Change button color to light purple
+                      ),
+                      child: Text('Course 1'),
+                      onPressed: () {
+                        setState(() {
+                          _selectedCourse = 'Course 1';
+                        });
+                      }, // Add button press handler
+                    ),
+                  ),
+                  SizedBox(width: 20), // Add space between button and text
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      _selectedCourse == 'Course 1'
+                          ? 'This is Course 1. Learn the basics of programming.'
+                          : '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: _buttonSpacing), // Add space between buttons
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(20), // Increase button padding
+                        textStyle:
+                            TextStyle(fontSize: 24), // Increase text size
+                        backgroundColor: Colors
+                            .purple[200], // Change button color to light purple
+                      ),
+                      child: Text('Course 2'),
+                      onPressed: () {
+                        setState(() {
+                          _selectedCourse = 'Course 2';
+                        });
+                      }, // Add button press handler
+                    ),
+                  ),
+                  SizedBox(width: 20), // Add space between button and text
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      _selectedCourse == 'Course 2'
+                          ? 'This is Course 2. Learn advanced programming concepts.'
+                          : '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: _buttonSpacing), // Add space between buttons
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(20), // Increase button padding
+                        textStyle:
+                            TextStyle(fontSize: 24), // Increase text size
+                        backgroundColor: Colors
+                            .purple[200], // Change button color to light purple
+                      ),
+                      child: Text('Course 3'),
+                      onPressed: () {
+                        setState(() {
+                          _selectedCourse = 'Course 3';
+                        });
+                      }, // Add button press handler
+                    ),
+                  ),
+                  SizedBox(width: 20), // Add space between button and text
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      _selectedCourse == 'Course 3'
+                          ? 'This is Course 3. Learn specialized programming skills.'
+                          : '',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                 ],
               ),
