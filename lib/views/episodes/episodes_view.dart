0cadce03067230ceec0ca72ad8af5dd245e16d66
Tarun-Rajan <ktarunrajan@gmail.com>
2024-07-25 16:56:12 +0530

    Update episodes_view.dart
    episodes view

--- lib/views/episodes/episodes_view.dart
+++ lib/views/episodes/episodes_view.dart
@@ -7,41 +7,63 @@ class EpisodesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white, // Background color changed to white
-      child: Center(
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center, // Center the column vertically
-          children: [
-            SizedBox(height: _buttonSpacing), // Add some space at the top
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(20), // Increase button padding
-                textStyle: TextStyle(fontSize: 24), // Increase text size
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Episodes'),
+        backgroundColor: Colors.purple, // Add a purple app bar
+      ),
+      body: Container(
+        color: Colors.white, // Background color changed to white
+        child: Padding(
+          padding:
+              const EdgeInsets.all(20.0), // Add some padding around the content
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Center the column vertically
+            children: [
+              Text(
+                'Select an Episode',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
-              child: Text('course 1'),
-              onPressed: () {}, // Add button press handler
-            ),
-            SizedBox(height: _buttonSpacing), // Add space between buttons
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(20), // Increase button padding
-                textStyle: TextStyle(fontSize: 24), // Increase text size
+              SizedBox(height: _buttonSpacing), // Add some space at the top
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20), // Increase button padding
+                  textStyle: TextStyle(fontSize: 24), // Increase text size
+                  backgroundColor:
+                      Colors.white, // Change button color to purple
+                ),
+                child: Text('Course 1'),
+                onPressed: () {}, // Add button press handler
               ),
-              child: Text('course 2'),
-              onPressed: () {}, // Add button press handler
-            ),
-            SizedBox(height: _buttonSpacing), // Add space between buttons
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(20), // Increase button padding
-                textStyle: TextStyle(fontSize: 24), // Increase text size
+              SizedBox(height: _buttonSpacing), // Add space between buttons
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20), // Increase button padding
+                  textStyle: TextStyle(fontSize: 24), // Increase text size
+                  backgroundColor:
+                      Colors.white, // Change button color to purple
+                ),
+                child: Text('Course 2'),
+                onPressed: () {}, // Add button press handler
               ),
-              child: Text('course 3'),
-              onPressed: () {}, // Add button press handler
-            ),
-          ],
+              SizedBox(height: _buttonSpacing), // Add space between buttons
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20), // Increase button padding
+                  textStyle: TextStyle(fontSize: 24), // Increase text size
+                  backgroundColor:
+                      Colors.white, // Change button color to purple
+                ),
+                child: Text('Course 3'),
+                onPressed: () {}, // Add button press handler
+              ),
+            ],
+          ),
         ),
       ),
     );
