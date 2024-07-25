1a5f014ebde54fa93dc2e7d7158d6a1a5fa7cf32
Tarun-Rajan <ktarunrajan@gmail.com>
2024-07-25 16:18:02 +0530

    appearance
    about us chnages

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -5,8 +5,49 @@ class AboutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Us'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/taskbg.jpg'), // Add a background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage:
+                    AssetImage('assets/fluttelogo.webp'), // Add a logo
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to our app!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'This app is designed to help you learn new things and have fun while doing it.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
